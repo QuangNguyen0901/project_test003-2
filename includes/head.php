2fcf1a02c9a42f38686fa22fdec38f64874ee5d2
Nguyen MinhQuang <Nguyen MinhQuang>
2016-11-21 23:11:13 +0700

    sua duong dan
    sua duong dan css

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>View</title>
-    <link rel="stylesheet" href="/AhKhuyenMai/css/style.css"/>
+    <link rel="stylesheet" href="./css/style.css"/>
 </head>
 <?php $url_common_1 = "http://localhost:8888/AhKhuyenMai/";?>
 
