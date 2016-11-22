043d244022e2445dc2776cfca376863ba2d498e4
QuangNguyen0901 <hikari0901@gmail.com>
2016-11-22 11:32:55 +0700

    2

--- includes/admin_head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>ADMIN</title>
-    <link rel="stylesheet" href="/AhKhuyenMai/css/admin_style.css"/>
-
-</head>
--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-    <p>copyright-2016</p>
-</div><!--end div "footer"-->
-
-
