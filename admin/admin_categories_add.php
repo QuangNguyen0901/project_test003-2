381c361faa2f37abae857f31416829d10568b320
QuangNguyen0901 <hikari0901@gmail.com>
2016-12-21 14:55:23 +0700

    aa

--- admin/admin_categories_add.php
+++ admin/admin_categories_add.php
@@ -30,7 +30,7 @@
                     }
 
                     if (empty($errors)) {
-                        $q = "INSERT INTO categories (user_id, c_name, c_icon, c_position) VALUES (1,'{$cat_name}','{$icon}',$position)";
+                        $q = "INSERT INTO categories (user_id, c_name, c_icon, position) VALUES (1,'{$cat_name}','{$icon}',$position)";
                         $r = mysqli_query($conn, $q) or die("Query {$q} \n<br/> MySQL Error: " . mysqli_error($conn));
                         if (mysqli_affected_rows($conn) == 1) {
                             $messages = "<p>Đã đăng ký category thành công</p>";
@@ -62,17 +62,23 @@
                         echo "<p class='warning'>Hãy chọn icon</p>";
                     }
                     ?>
-                    
+
                     <p>Chọn vị trí hiển thị</p>
                     <select name="position">
+                        <option value="1"></option>
                         <?php
                         $q = "SELECT count(categories_id) AS count FROM categories";
                         $r = mysqli_query($conn, $q) or die("Query {$q} \n<br/> MySQL Error: " . mysqli_error($conn));
                         if (mysqli_num_rows($r) == 1) {
-                            $num = mysqli_fetch_array($r, MYSQLI_NUM);
+                            list($num) = mysqli_fetch_array($r, MYSQLI_NUM);
                             for ($i = 1; $i <= $num + 1; $i++) {
                                 echo "<option value='{$i}'";
-                                if (isset($_POST['position']) AND $_POST['position'] == $i) echo "selected ='selected'";
+                                if (isset($_POST['position']) AND $_POST['position'] == $i){
+                                    echo "selected ='selected'";
+                                }elseif (empty ($_POST['position'])AND $i==$num + 1){
+                                    echo "selected ='selected'";
+                                }
+
                                 echo ">" . $i . "</option>";
                             }
                         }
