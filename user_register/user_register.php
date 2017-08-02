69c8fec85fe2e3413611d70ed61739509f74c5c4
Quang HP <hikari0901@gmail.com>
2017-08-02 14:46:20 +0700

    aa

--- user_register/user_register.php
+++ user_register/user_register.php
@@ -11,32 +11,36 @@
 <body>
 <?php include("../includes/mysqli_connect.php"); ?>
 <?php
-if (isset($_POST['confirm'])){
+if (isset($_POST['confirm'])) {
     $error = array();
-    if (empty($_POST['uname'])){
-        $error[]="user_name";
-    }else{
+    if (empty($_POST['uname'])) {
+        $error[] = "user_name";
+
+    } else {
         $uname = mysqli_real_escape_string($conn, strip_tags($_POST['uname']));
     }
-    if (empty($_POST['pass'])){
-        $error[]="pass";
-    }else{
+    if (empty($_POST['pass'])) {
+        $error[] = "pass";
+    } else {
         $pass = mysqli_real_escape_string($conn, strip_tags($_POST['pass']));
     }
-    if (empty($_POST['confirm_pass']))or ($_POST !={
-        $error[]="confirm_pass";
-    }else{
+    if (empty($_POST['confirm_pass'])) {
+        $error[] = "confirm_pass";
+    } else if ($_POST['pass'] != $_POST['confirm_pass']) {
+        $error[] = "confirm_pass2";
 ////////////////////////////////////////
     }
-    if (empty($_POST['sex'])){
-        $error[]="sex";
-    }else{
+    if (empty($_POST['sex'])) {
+        $error[] = "sex";
+    } else {
         $sex = mysqli_real_escape_string($conn, strip_tags($_POST['sex']));
     }
+//    print_r($_POST);
+    print_r($error);
 }
 
-
 ?>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12" style="background-color:lavender;">TOP</div>
@@ -45,35 +49,41 @@ if (isset($_POST['confirm'])){
         <div class="col-md-2">LEFT</div>
         <div class="col-md-8" style="background-color: #ffbe87;">
             <h4>Member Register</h4>
-            <form method="post">
+            <form method="post" action="user_register.php" name="register_form">
                 <div class="form-group">
                     <label>User Name:</label>
                     <input type="text" class="form-control" name="uname" id="usr">
+                    <?php
+                    if (isset($_POST['confirm'])) {
+                        if (in_array("user_name", $error)) {
+                            echo "<p style='color: red'>Please input username</p>";
+                        }
+                    }
+                    ?>
                 </div>
                 <div class="form-group">
                     <label>Password:</label>
                     <input type="password" class="form-control" name="pass" id="pwd">
+                    <p id="validate_pass" style="color: greenyellow"></p>
                 </div>
                 <div class="form-group">
                     <label>Confirm Password:</label>
-                    <input type="password" class="form-control" name="confirm_pass" id="pwd">
+                    <input type="password" class="form-control" name="confirm_pass" id="pwd1">
                 </div>
                 <div class="form-group">
                     <label>Sex:
-                        <form>
-                            <label class="radio-inline">
-                                <input type="radio" name="sex" value="1">Male
-                            </label>
-                            <label class="radio-inline">
-                                <input type="radio" name="sex" value="2">Female
-                            </label>
-                        </form>
+                        <label class="radio-inline">
+                            <input type="radio" name="sex" value="1">Male
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="sex" value="2">Female
+                        </label>
                     </label>
                 </div>
-                <div class="checkbox">
-                    <label><input type="checkbox"> Remember me</label>
-                </div>
-                <button type="submit" class="btn btn-primary" name="confirm">Confirm</button>
+                <!--                <div class="checkbox">-->
+                <!--                    <label><input type="checfsdfsdfdskbox"> Remember me</label>-->
+                <!--                </div>-->
+                <button type="submit" class="btn btn-primary" id="aaaa" name="confirm">Confirm</button>
             </form>
         </div>
         <div class="col-md-2">RIGHT</div>
@@ -82,6 +92,33 @@ if (isset($_POST['confirm'])){
         <div class="col-md-12" style="background-color:lavender;">BOTTOM</div>
     </div>
 </div>
+<p onclick="validate_password()" >aaaaaa</p>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+<script>
+
+    $("#aaaa").click(function() {
+        return validate_password();
+//        var a = document.getElementById("validate_pass");
+//        var x = document.getElementById("pwd").value;
+//        if (x==""){
+//            a.innerHTML="Please input password";
+//            return false;
+//        }
+    });
+    function validate_password() {
+
+        var a = document.getElementById("validate_pass");
+        var x = document.getElementById("pwd").value;
+        if (x==""){
+            a.innerHTML="Please input password";
+            return false;
+        }
+
+
+
+    }
+</script>
 
 </body>
 </html>
