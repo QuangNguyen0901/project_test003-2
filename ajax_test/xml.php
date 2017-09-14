b1599e66909761e0ecb197342f4938c357c9b450
Quang HP <hikari0901@gmail.com>
2017-09-14 11:23:32 +0700

    aaa

--- ajax_test/xml.php
+++ ajax_test/xml.php
@@ -1,13 +1,13 @@
-<?php include('../includes/mysqli_connect.php'); ?>
+<?php
+header('Content-type: text/xml');
+echo '<?xml version="1.0" encoding="utf-8" ?>'; //Dinh nghia nay phai dat tren dau trang,chu y file include
+?>
 
+<?php include('../includes/mysqli_connect.php'); ?>
 <?php
 $query = mysqli_query($conn,'select user_id,username from user ');
 
-header('Content-type: text/xml');
-echo '<?xml version="1.0" encoding="utf-8" ?>';
-
 echo '<root>';
-
 if (mysqli_num_rows($query)>0)
 {
     while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
